ee3890850acebff94900ff78594388d7e1fdfc2e
sazidtalukdar <47768520+sazidtalukdar@users.noreply.github.com>
2024-08-01 23:00:58 +0600

    static

---
+++ src/main/resources/static/st.js
@@ -0,0 +1,38 @@
+
+const form = document.getElementById('portForm');
+form.addEventListener('submit', function(check){
+    check.preventDefault();
+
+    const port= parseInt(document.getElementById('port').value);
+    const host = document.getElementById('host').value;
+
+    fetch('https://stport.azurewebsites.net/check-port',{
+        method :'POST',
+        headers :{
+            'content-type' : 'application/json',
+        },
+        body : JSON.stringify({host,port}),
+    })
+
+    .then(response => { 
+        if(!response.ok){
+        throw new Error('Not Ok');
+        }
+        return response.json();
+
+    })
+
+    .then(data =>{
+        const result = document.getElementById('result');
+        if(data.open){
+            result.innerText=port + " Is Open";
+
+        }else{
+            result.innerText=port + " Is Closed";
+
+        }
+    })
+    
+    
+
+});
